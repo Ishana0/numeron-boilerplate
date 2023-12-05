9ebdab7ce08ed38455534e7df4f2f6722fd908f7
Ishana0 <ishanakalikiri01@gmailcom>
2023-12-05 16:25:29 +0530

    numeron

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 // Iteration 1: Make the Play button functional.
 // Description: When the Play button is clicked the game page should be displayed.
+
+const PlayButton = document.getElementById("play-button");
+
+PlayButton.onclick = () => {
+
+    window.location.href="./game.html";
+
+};
+
+
